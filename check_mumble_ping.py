d682bb01a2151c084a1d5895bef42eae68eb0b22
Markus Opolka <markus@martialblog.de>
2023-08-02 13:00:09 +0200

    Fix indentation issues

--- check_mumble_ping.py
+++ check_mumble_ping.py
@@ -41,66 +41,63 @@ def return_plugin(status, msg):
     return status
 
 def ping_mumble(host, port=64738):
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.settimeout(1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(1)
 
-  buf = pack(">iQ", 0, datetime.datetime.now().microsecond)
-  s.sendto(buf, (host, port))
+    buf = pack(">iQ", 0, datetime.datetime.now().microsecond)
+    s.sendto(buf, (host, port))
 
-  data, _ = s.recvfrom(1024)
+    data, _ = s.recvfrom(1024)
 
-  r = unpack(">bbbbQiii", data)
+    r = unpack(">bbbbQiii", data)
 
-  version = r[1:4]
-  # r[0,1,2,3] = version
-  # r[4] = ts
-  # r[5] = users
-  # r[6] = max users
-  # r[7] = bandwidth
+    version = r[1:4]
+    # r[0,1,2,3] = version
+    # r[4] = ts
+    # r[5] = users
+    # r[6] = max users
+    # r[7] = bandwidth
 
-  ping = (datetime.datetime.now().microsecond - r[4]) / 1000.0
-  if ping < 0:
-      ping = ping + 1000
+    ping = (datetime.datetime.now().microsecond - r[4]) / 1000.0
+    if ping < 0:
+        ping = ping + 1000
 
-  return {
-    "version": version,
-    "user": r[5:7],
-    "time": ping,
-    "rate": r[7],
-    "len": len(data)
-  }
+    return {
+        "version": version,
+        "user": r[5:7],
+        "time": ping,
+        "rate": r[7],
+        "len": len(data)
+    }
 
 def main():
-  scriptname = os.path.basename(sys.argv[0])
+    scriptname = os.path.basename(sys.argv[0])
 
-  parser = argparse.ArgumentParser(prog=scriptname)
+    parser = argparse.ArgumentParser(prog=scriptname)
 
-  parser.add_argument('-H', '--host', type=str, required=True,
-                  help='Mumble host')
-  parser.add_argument('-p', '--port', type=int, required=False,
-                  default=64738,
-                  help='Mumble port (default is 64738')
+    parser.add_argument('-H', '--host', type=str, required=True,
+                        help='Mumble host')
+    parser.add_argument('-p', '--port', type=int, required=False,
+                        default=64738,
+                        help='Mumble port (default is 64738')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  try:
+    try:
+        ping = ping_mumble(args.host, args.port)
+        items = [
+            "version={0[0]}.{0[1]}.{0[2]}".format(ping["version"]),
+            "user={0[0]}/{0[1]}".format(ping["user"]),
+            "time={:.2f}ms".format(ping["time"]),
+            "rate={:.2f}kbit/s".format(ping["rate"]/1000),
+            "len={:d}b".format(ping["len"])
+        ]
+        return return_plugin(0, ", ".join(items))
 
-    ping = ping_mumble(args.host, args.port)
+    except Exception as e: # pylint: disable=broad-except
+        return return_plugin(3, "Error: could not send ping ({0})".format(e) )
 
-    items = [
-      "version={0[0]}.{0[1]}.{0[2]}".format(ping["version"]),
-      "user={0[0]}/{0[1]}".format(ping["user"]),
-      "time={:.2f}ms".format(ping["time"]),
-      "rate={:.2f}kbit/s".format(ping["rate"]/1000),
-      "len={:d}b".format(ping["len"])
-    ]
-
-    return return_plugin(0, ", ".join(items))
-
-  except Exception as e: # pylint: disable=broad-except
-    return return_plugin(3, "Error: could not send ping ({0})".format(e) )
-
-  return 0
+    return 0
 
 if __name__ == "__main__": # pragma: no cover
     sys.exit(main())
